ebcc7dffe7387bcde6440831b2fd81d50771ced9
justinacer-hub <justinacerda10@gmail.com>
2026-03-20 13:53:33 -0300

    primer inciso

--- question.py
+++ question.py
@@ -36,7 +36,11 @@ while attempts > 0:
     print(f"Letras usadas: {', '.join(guessed)}")
 
     letter = input("Ingresá una letra: ")
-
+    
+    if len(letter) != 1 or not letter.isalpha():
+        print("Entrada no valida")
+        continue
+    
     if letter in guessed:
         print("Ya usaste esa letra.")
     elif letter in word:
